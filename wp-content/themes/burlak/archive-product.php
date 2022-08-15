f30d0cd64d2b5ff5c4a93772029537c34bda269f
Eugene Burlak <burlakeugene@gmail.com>
2022-08-16 00:15:06 +0300

    proudcts archive, cats

--- wp-content/themes/burlak/archive-product.php
+++ wp-content/themes/burlak/archive-product.php
@@ -1,18 +1,26 @@
 <?php
+$page = get_page_by_path('products');
+$id = $page->ID;
+$path = 'product/categories';
+if($_GET){
+  $path = 'product/list';
+}
+
 get_header();
-  my_get_template_part('sections/header', array(
-    'title' => array(
-      'text' => get_post_type_object(get_post_type())->labels->name,
-    ),
-    'breadcrumbs' => true
-  ));
-?>
-<div class="container">
-  <?php get_template_part('product/categories'); ?>
-  <?php my_get_template_part('product/list', array(
-    'classes' => ['products--archive']
-  )); ?>
-</div>
-<?php
+  my_get_template_part('blocks/decorator_background', [
+    'background' => get_field('decorator_background', $id)['sizes']['large']
+  ]);
+  my_get_template_part('sections/section', [
+    'classes' => ['decorator_background_next'],
+    'header' => [
+      'breadcrumbs' => true,
+      'title' => [
+        'text' => get_post_type_object(get_post_type())->labels->name,
+      ],
+    ],
+    'content' => [
+      'path' => $path
+    ]
+  ]);
 get_footer();
 ?>
