dbf57fbe5dd6b603c493de67db7ebb213881abc2
Eugene Burlak <burlakeugene@gmail.com>
2022-11-21 23:57:27 +0300

    upd

--- wp-content/themes/burlak/src/js/router.js
+++ wp-content/themes/burlak/src/js/router.js
@@ -148,8 +148,13 @@
         Promise.all(
           [...media].map((item) => {
             return new Promise((resolve, reject) => {
-              item.onload = resolve;
-              if (item.tagName === 'IMG') item.onload = resolve;
+              if (item.tagName === 'IMG') {
+                item.onload = resolve;
+                item.onerror = () => {
+                  console.warn('Warning, media can\'t resolve: ', item);
+                  resolve();
+                };
+              }
               if (item.tagName === 'VIDEO') {
                 item.addEventListener('loadedmetadata', () => {
                   resolve();
