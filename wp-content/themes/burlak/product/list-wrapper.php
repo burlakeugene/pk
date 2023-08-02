88c4b017f81c8370cf7e23a760c8d29098609872
Eugene Burlak <burlakeugene@gmail.com>
2023-08-02 21:51:30 +0300

    upd

--- wp-content/themes/burlak/product/list-wrapper.php
+++ wp-content/themes/burlak/product/list-wrapper.php
@@ -1,9 +1,7 @@
 <div class="products__with-filters">
   <?php
   $filters = getFilters();
-  if($_GET['xyu']){
-    my_get_template_part('product/filter-steps', $filters);
-  }
+  my_get_template_part('product/filter-steps', $filters);
   my_get_template_part('product/filter-top');
   my_get_template_part('product/filter', $filters);
   my_get_template_part('product/list', [
