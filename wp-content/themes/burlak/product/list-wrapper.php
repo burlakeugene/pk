936480350ff54bc114c5032ea9b9e546d10d2067
Eugene Burlak <burlakeugene@gmail.com>
2024-04-16 15:18:34 +0300

    hard fix

--- wp-content/themes/burlak/product/list-wrapper.php
+++ wp-content/themes/burlak/product/list-wrapper.php
@@ -1,6 +1,9 @@
 <div class="products__with-filters">
   <?php
   $filters = getFilters();
+  ?>
+  <div style="display: none;"><?php print_r($filters); ?></div>
+  <?php
   my_get_template_part('product/filter-steps', $filters);
   my_get_template_part('product/filter-top');
   my_get_template_part('product/filter', $filters);
