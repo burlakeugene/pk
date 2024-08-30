1d61db26bb50ad1d770d9e0cce0d0276d1fb5f5c
Eugene Burlak <burlakeugene@gmail.com>
2024-08-30 11:48:20 +0300

    index switch

--- wp-content/themes/burlak/index.php
+++ wp-content/themes/burlak/index.php
@@ -2,40 +2,18 @@
   get_header();
   my_get_template_part('banners/index');
 
-  $brand_id = get_page_by_path('brands')->ID;
-
   my_get_template_part('sections/section', [
     'classes' => ['decorator_background_next'],
     'header' => [
       'title' => [
-        'text' => get_fields($brand_id)['title_index'],
-        'mini' => true,
-        'apperance' => ['light', 'theme'],
-      ],
-      'link' => [
-        'text' => 'Все бренды',
-        'ajax' => true,
-        'href' => get_permalink($brand_id),
-      ]
-    ],
-    'content' => [
-      'path' => 'brands/index',
-    ]
-  ]);
-
-  my_get_template_part('sections/section', [
-    'classes' => ['decorator_background', 'decorator_background--gray'],
-    'header' => [
-      'title' => [
         'text' => get_post_type_object('product')->labels->name,
         'mini' => true,
-        'apperance' => ['dark', 'theme'],
+        'apperance' => ['light', 'theme'],
       ],
       'link' => [
         'text' => 'Все категории',
         'ajax' => true,
         'href' => get_permalink(get_page_by_path('categories')->ID),
-        'modificators' => ['dark'],
       ]
     ],
     'content' => [
@@ -43,6 +21,27 @@
       'props' => [
         'index' => true
       ]
+    ]
+  ]);
+
+  $brand_id = get_page_by_path('brands')->ID;
+  my_get_template_part('sections/section', [
+    'classes' => ['decorator_background', 'decorator_background--gray'],
+    'header' => [
+      'title' => [
+        'text' => get_fields($brand_id)['title_index'],
+        'mini' => true,
+        'apperance' => ['dark', 'theme'],
+      ],
+      'link' => [
+        'text' => 'Все бренды',
+        'ajax' => true,
+        'href' => get_permalink($brand_id),
+        'modificators' => ['dark'],
+      ]
+    ],
+    'content' => [
+      'path' => 'brands/index',
     ]
   ]);
 
